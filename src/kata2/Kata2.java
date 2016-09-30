2af9ada88e5fbc5824750c99e19c7f201c0ca897
usuario <usuario@10.22.143.54>
2016-09-30 11:55:40 +0100

    version2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,10 +5,15 @@ import java.util.HashMap;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] array = {1,4,7,1,1,8,4,1,4,9};
+        //Integer[] array = {1,4,7,1,1,8,4,1,4,9};
+        //Double[] array = {1.1,4.3,7.4,1.1,1.2,8.2,4.4,1.2,4.3,9.5};
+        String[] array = {"Pepe", "Juan", "Pepe", "Dani", "Pepe"};
+        
         Histogram histo = new Histogram(array);
-        HashMap <Integer,Integer> histogram = histo.getHistogram();
-        for(int key: histogram.keySet()){
+        
+        HashMap <Object,Integer> histogram = histo.getHistogram();
+        
+        for(Object key: histogram.keySet()){
             System.out.println(key + "->" + histogram.get(key));
         }
     }
